7de9544430d79a2bfdc7b0f860d4c447cc9242d8
Matthew Finerty <mfinerty@cpp.edu>
2022-10-19 19:37:00 -0700

    Implement checking that each test case is correct

--- employeeMain.c
+++ employeeMain.c
@@ -18,11 +18,39 @@ int main(void) {
     PtrToEmployee employeeSearchPhoneNumberPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "310-555-1215");
     PtrToEmployee employeeSearchSalaryPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 6.34);
     
+    // Check that each search was successful
+    PtrToEmployee searches[] = { employeeSearchNumberPtr, employeeSearchNamePtr, employeeSearchPhoneNumberPtr, employeeSearchSalaryPtr };
+    for (int i = 0; i < 4; i++) {
+        PtrToEmployee employee = searches[i]; // Get pointer to employee from array
+        if (employee == NULL) {
+            // Search function is not working properly; exit program
+            printf("Could not find employee %d; exiting...\n", i + 1);
+            return EXIT_FAILURE;
+        } else {
+            // Employee found; search works correctly
+            printf("Found employee %d-%d: %s (Phone number: %s, salary: %.2f)\n", i + 1, employee->number, employee->name, employee->phone, employee->salary);
+        }
+    }
+
     // Test each function when employee is not found
     PtrToEmployee employeeSearchNumberPtrFail = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 7777);
     PtrToEmployee employeeSearchNamePtrFail = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Matthew Finerty");
     PtrToEmployee employeeSearchPhoneNumberPtrFail = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "123-456-7890");
     PtrToEmployee employeeSearchSalaryPtrFail = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 0.00);
+
+    // Check that each search was unsuccessful
+    PtrToEmployee searchesFail[] = { employeeSearchNumberPtrFail, employeeSearchNamePtrFail, employeeSearchPhoneNumberPtrFail, employeeSearchSalaryPtrFail };
+    for (int i = 0; i < 4; i++) {
+        PtrToEmployee employee = searchesFail[i]; // Get pointer to employee from array
+        if (employee != NULL) {
+            // Search function is not working properly; exit program
+            printf("Found employee %d; exiting...\n", i + 1);
+            return EXIT_FAILURE;
+        } else {
+            // Employee not found; search properly returns null
+            printf("Could not find employee %d (success)\n", i + 1);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
