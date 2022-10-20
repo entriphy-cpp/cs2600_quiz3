fca24e8e21b3a949607b0800b0935c2fe4ae3cff
Matthew Finerty <mfinerty@cpp.edu>
2022-10-19 19:19:00 -0700

    Create initial code outline

---
+++ employeeMain.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+// External table
+
+// External functions
+
+int main(void)
+{
+    // Test each function when employee is found
+
+    // Test each function when employee is not found
+
+    return EXIT_SUCCESS;
+}
