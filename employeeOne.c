c692088123aba249af682b2259706a5ce54e8dea
Matthew Finerty <mfinerty@cpp.edu>
2022-10-19 19:43:00 -0700

    Implement search employee by phone number and salary

--- employeeOne.c
+++ employeeOne.c
@@ -22,3 +22,25 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
     }
     return NULL; // Could not find employee matching name; employee could not be found
 }
+
+// Search for an employee by phone number
+PtrToEmployee searchEmployeeByPhoneNumber(PtrToConstEmployee ptr, int tableSize, char *targetPhoneNumber) {
+    const PtrToConstEmployee endPtr = ptr + tableSize; // Calculate pointer to the end of the table
+    for (; ptr < endPtr; ptr++) { // Iterate through employee table
+        if (strcmp(ptr->phone, targetPhoneNumber) == 0) { // Employee phone number in current iteration equals target phone number
+            return (PtrToEmployee) ptr; // Return pointer to found employee
+        }
+    }
+    return NULL; // Could not find employee matching phone number; employee could not be found
+}
+
+// Search for an employee by salary
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary) {
+    const PtrToConstEmployee endPtr = ptr + tableSize; // Calculate pointer to the end of the table
+    for (; ptr < endPtr; ptr++) { // Iterate through employee table
+        if (ptr->salary == targetSalary) { // Employee salary in current iteration equals target salary
+            return (PtrToEmployee) ptr; // Return pointer to found employee
+        }
+    }
+    return NULL; // Could not find employee matching salary; employee could not be found
+}
