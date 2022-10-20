612d11618c7bc7e94b88095b05643afdaf81707a
Matthew Finerty <mfinerty@cpp.edu>
2022-10-19 19:50:00 -0700

    Implement search and comparison functions for employee phone number and salary

--- employeeTwo.c
+++ employeeTwo.c
@@ -22,6 +22,14 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
     return strcmp((char *) targetPtr, tableValuePtr->name);
 }
 
+static int compareEmployeePhoneNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr) {
+    return strcmp((char *) targetPtr, tableValuePtr->phone);
+}
+
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr) {
+    return *(double *) targetPtr != tableValuePtr->salary;
+}
+
 /* Wrapper functions (uses comparison functions, used in employeeMain) */
 
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number) {
@@ -30,4 +38,12 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long numb
 
 PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char *name) {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+
+PtrToEmployee searchEmployeeByPhoneNumber(PtrToConstEmployee ptr, int size, char *phoneNumber) {
+    return searchEmployeeTable(ptr, size, phoneNumber, compareEmployeePhoneNumber);
+}
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary) {
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary);
 }
