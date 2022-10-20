e2d566ccfabd8f6ea620b487381843df61c6231b
Matthew Finerty <mfinerty@cpp.edu>
2022-10-19 19:25:00 -0700

    Define external table and functions in main file

--- employeeMain.c
+++ employeeMain.c
@@ -2,8 +2,14 @@
 #include "employee.h"
 
 // External table
+extern Employee EmployeeTable[];
+extern const int EmployeeTableEntries;
 
 // External functions
+PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
+PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char *nameToFind);
+PtrToEmployee searchEmployeeByPhoneNumber(const Employee table[], int sizeTable, char *phoneNumberToFind);
+PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double *salary);
 
 int main(void)
 {
