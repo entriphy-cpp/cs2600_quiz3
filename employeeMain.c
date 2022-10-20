43480603db2cd2e8b7938b9ef2789be54ffc4f36
Matthew Finerty <mfinerty@cpp.edu>
2022-10-19 19:30:00 -0700

    Create variables for each test case

--- employeeMain.c
+++ employeeMain.c
@@ -9,13 +9,20 @@ extern const int EmployeeTableEntries;
 PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
 PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char *nameToFind);
 PtrToEmployee searchEmployeeByPhoneNumber(const Employee table[], int sizeTable, char *phoneNumberToFind);
-PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double *salary);
+PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double salaryToFind);
 
-int main(void)
-{
+int main(void) {
     // Test each function when employee is found
-
+    PtrToEmployee employeeSearchNumberPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1001);
+    PtrToEmployee employeeSearchNamePtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tammy Franklin");
+    PtrToEmployee employeeSearchPhoneNumberPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "310-555-1215");
+    PtrToEmployee employeeSearchSalaryPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 6.34);
+    
     // Test each function when employee is not found
+    PtrToEmployee employeeSearchNumberPtrFail = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 7777);
+    PtrToEmployee employeeSearchNamePtrFail = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Matthew Finerty");
+    PtrToEmployee employeeSearchPhoneNumberPtrFail = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "123-456-7890");
+    PtrToEmployee employeeSearchSalaryPtrFail = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 0.00);
 
     return EXIT_SUCCESS;
 }
